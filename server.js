63aa6290f616c5a752b433b19e1da3883a198606
mohammed5212 <mohammedchpktr@gmail.com>
2025-08-01 07:45:56 +0530

    added files

---
+++ server.js
@@ -0,0 +1,16 @@
+const express =require('express')
+const app =express()
+
+
+const PORT=3000
+
+
+app.get('/',(req,res)=>{
+    res.send("hello")
+
+
+})
+
+    app.listen(PORT,()=>{
+        console.log(`server s running on ${PORT} port`)
+    })
